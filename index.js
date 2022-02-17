fee4bbc2eae82c1a01aeae48440aff0500642735
Benjamin <benjamin@macbook-pro-de-benjamin.home>
2022-02-17 15:34:04 +0100

    code optimisation

--- index.js
+++ index.js
@@ -3,22 +3,35 @@ const express = require("express");
 const formidable = require("express-formidable");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
 
 const app = express();
 app.use(cors());
 app.use(formidable());
 
-// Import des routes
-
-const userRoutes = require("./route/user");
-app.use(userRoutes);
-const offerRoutes = require("./route/offer.");
-app.use(offerRoutes);
-
 mongoose.connect(process.env.BDD_ADRESS, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+// Import des routes
+
+const userRoutes = require("./route/user");
+app.use(userRoutes);
+const offerRoutes = require("./route/offer");
+app.use(offerRoutes);
+const paymentRoutes = require("./route/payment");
+app.use(paymentRoutes);
+
+app.get("/", (req, res) => {
+  res.json("Welcome on Vinted API");
 });
 
 app.all("*", (req, res) => {
--- route/user.js
+++ route/user.js
@@ -4,27 +4,16 @@ const router = express.Router();
 const uid2 = require("uid2");
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
-});
+// const cloudinary = require("cloudinary").v2;
 
 // Import model
 
 const User = require("../models/user");
+const Offer = require("../models/offer");
 
 // Routes
 
 router.post("/user/signup", async (req, res) => {
-  const salt = uid2(16);
-
-  const hash = SHA256(req.fields.password + salt).toString(encBase64);
-
-  const token = uid2(16);
-
   try {
     const user = await User.findOne({ email: req.fields.email });
 
@@ -34,22 +23,27 @@ router.post("/user/signup", async (req, res) => {
       });
     } else {
       if (req.fields.email && req.fields.username && req.fields.password) {
+        const salt = uid2(16);
+        const hash = SHA256(req.fields.password + salt).toString(encBase64);
+        const token = uid2(16);
+
         const newUser = new User({
           email: req.fields.email,
           account: {
             username: req.fields.username,
             phone: req.fields.phone,
           },
+          newsletter: req.fields.newsletter,
           hash: hash,
           token: token,
           salt: salt,
         });
 
-        await cloudinary.uploader.upload(req.files.avatar, {
-          folder: "/vinted/user",
-          public_id: "avatar",
-        });
-        newUser.account.avatar = result;
+        // await cloudinary.uploader.upload(req.files.avatar, {
+        //   folder: "/vinted/user",
+        //   public_id: "avatar",
+        // });
+        // newUser.account.avatar = result;
 
         await newUser.save();
         res.status(200).json({
@@ -69,16 +63,11 @@ router.post("/user/signup", async (req, res) => {
 
 router.post("/user/login", async (req, res) => {
   try {
-    // const logIn = (req.fields.email, req.fields.password);
-    const email = req.fields.email;
-    const user = await User.findOne({ email: email });
-    // console.log(user);
-    const password = req.fields.password;
-    const userSalt = await user.salt;
-    const userHash = await user.hash;
+    const user = await User.findOne({ email: req.fields.email });
 
-    const hash = SHA256(password + userSalt).toString(encBase64);
     if (user) {
+      const userHash = await user.hash;
+      const hash = SHA256(req.fields.password + user.salt).toString(encBase64);
       if (userHash === hash) {
         res.status(200).json({
           _id: user._id,
